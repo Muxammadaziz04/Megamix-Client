a9efcdad4fbbacf981c689003d8aa1db06d67214
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-07-30 21:10:31 +0500

    social graphic

--- src/components/SEO/settings.js
+++ src/components/SEO/settings.js
@@ -2,9 +2,9 @@ const settings = {
     meta: {
         url: "https://megamix.getter.uz/",
         title: "Megamix",
-        description: "",
+        description: "Компания MEGAMIX сегодня - это перспективная и динамично развивающаяся компания, занимающаяся производством высокоэффективных сухих смесей и водоэмульсий.",
         social: {
-            graphic: "https://buzb.uz/Images/BrightUzbekistan.svg",
+            graphic: "http://megamix.uz/images/web_img/preveiw.jpg",
         },
         type: "website",
         keywords: "Megamix, Сухие смеси, Краски, Грунтовки, Клеи, строй материалы",
