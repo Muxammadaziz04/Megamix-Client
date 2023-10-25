a7234955b279e1d0ce4e4c653232311064f3eb3f
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-10-25 09:22:27 +0500

    Metriki dobavleni

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,35 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();
+                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                 
+                    ym(95359781, "init", {
+                         clickmap:true,
+                         trackLinks:true,
+                         accurateTrackBounce:true,
+                         webvisor:true
+                    });
+                    `
+                }}></script>
+
+
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-F4BNLZENTP"></script>
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                  
+                    gtag('config', 'G-F4BNLZENTP');
+                    `
+                }}>
+                </script>
             </body>
         </Html>
     )
